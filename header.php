f5e8282c17fe7cb46d374d09e6c0d87dc48ea11e
Vinicius Sakihara <vinicius.jp31@hotmail.com>
2016-09-23 00:34:17 +0000

    Html > php

---
+++ header.php
@@ -0,0 +1,18 @@
+
+        <header>
+            <div class="logo">
+                <h1><a href="index.html">Vizuu</a></h1>
+            </div>
+            <div class="nav">
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="community.html">Community</a></li>
+                        <li><a href="market.html">Store</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a id="modal_trigger" href="#modal">Login</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
